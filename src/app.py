5362d922145a3cc5d2ce312ab17448b36b49d976
Przemysław Palak <xut@bec.dk>
2021-03-15 16:13:19 +0100

    removing tpp id

--- src/app.py
+++ src/app.py
@@ -2,14 +2,16 @@ from openbanking_tpp_proxy.proxy import Proxy
 
 if __name__ == "__main__":
     #put api url here
-    apiUrl = ""
+    api_url = ""
+    #put tpp id from cert file here
+    tpp_id = ""
     proxy = Proxy("qwac_cert.cer", "qwac_key.key", "qseal_cert.cer", "qseal_key.key")
-    enrolmentResponse = proxy.enroll_certificates(apiUrl+"/eidas/1.0/v1/enrollment", "intermediate.cer", "root.cer", "DK-DFSA-5303", "Awsome tpp")
+    enrolmentResponse = proxy.enroll_certificates(api_url + "/eidas/1.0/v1/enrollment", "intermediate.cer", "root.cer", tpp_id, "Awsome tpp")
     print(enrolmentResponse.status_code)
     print(enrolmentResponse.content)
-    response = proxy.proxy_request("GET", apiUrl+"/eidas/1.0/v1/consents/health-check")
+    response = proxy.proxy_request("GET", api_url + "/eidas/1.0/v1/consents/health-check")
     print(response.status_code)
     print(response.content)
-    response = proxy.proxy_request("GET", apiUrl+"/eidas/1.0/v1/payments/health-check")
+    response = proxy.proxy_request("GET", api_url + "/eidas/1.0/v1/payments/health-check")
     print(response.status_code)
     print(response.content)
