cc29d9ea0cc2b23fbdf35b0d0631c868169221f5
Bernard Pietraga <bpietraga@protonmail.com>
2021-03-23 06:46:59 +0100

    Bump project version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="openbanking-tpp-client-python",
-    version="0.1.0",
+    version="0.2.0",
     author="Przemyslaw Palak",
     author_email="przemek@palak.pl",
     description="A tpp client library to call PSD2 API, taking care of security setup",
